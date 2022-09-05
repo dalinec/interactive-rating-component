fd7f18b8ca718a49d7e9b8ed9c86c77d370f5fce
Matej Kukolik <dalinec123@gmail.com>
2022-09-05 12:11:29 +0200

    add files

---
+++ script.js
@@ -0,0 +1,32 @@
+const submitBtn = document.querySelector('.submit');
+const cardOne = document.querySelector('.card-container-1');
+const cardTwo = document.querySelector('.card-container-2');
+const ratingBtns = document.querySelectorAll('.rating');
+const score = document.querySelector('.score');
+let starsScore = 3;
+
+const handleSubmit = () => {
+  cardOne.classList.add('hide');
+  score.textContent = starsScore;
+  cardTwo.classList.remove('hide');
+};
+
+submitBtn.addEventListener('click', handleSubmit);
+
+const handleRating = (event) => {
+  for (btn of ratingBtns) {
+    btn.classList.remove('active');
+  }
+
+  if (event.target.classList.contains('rating')) {
+    event.target.classList.add('active');
+  } else {
+    event.target.parentElement.classList.add('active');
+  }
+
+  starsScore = event.target.textContent;
+};
+
+for (btn of ratingBtns) {
+  btn.addEventListener('click', handleRating);
+}
